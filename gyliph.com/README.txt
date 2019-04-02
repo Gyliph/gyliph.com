473663d95cf803c1b1a6fa7bea4e0a17eda45a8f
Gyliph <jmuking@gmail.com>
2019-04-02 00:12:55 -0500

    Changing repo structure

--- gyliph.com/README.txt
+++
@@ -1,3 +0,0 @@
-This is Gyliph's web page
-
-//TODO: Add stuff here
---
+++ routeMe/config/config.js
@@ -0,0 +1,15 @@
+define({
+  "stravaDomain": "www.strava.com",
+  "stravaClientId": 27862,
+  "stravaClientSecret": "3c864905e003be23e0c5e1d813d234c82d502455",
+  "redirectUri": "https://gyliph.com/routeMe/",
+  "labelField": "name",
+  "routeService": "https://route.arcgis.com/arcgis/rest/services/World/Route/NAServer/Route_World",
+
+  "routeError": "Could not route from current extent, try a different one",
+  "routeErrorSelect": "Could not route from current selection, try a different one",
+  "logInError": "You are not logged in, please log in",
+
+  "routeNoSelect": "route based on segments in view",
+  "routeSelect": "route based on selected segments"
+})
